49dd8e65e2dae5a3956f13e9343f73d430424d9b
Анастасия Кудашева <gizmomogway@mail.ru>
2018-11-01 03:23:05 +0600

    Подготовка адаптивной графики в личном проекте

--- source/js/app.js
+++ source/js/app.js
@@ -14,3 +14,21 @@ document.querySelector(".week-offer__button").onclick = function () {
   document.querySelector(".modal--fixed").style.display = "flex";
   document.querySelector("add-form__button").style.display = "none"
 }
+
+ymaps.ready(init);
+var myMap;
+function init () {
+  myMap = new ymaps.Map("map", {
+    center: [59.938945, 30.323261],
+    behaviors: ['default', 'scrollZoom'],
+    zoom: 18
+  });
+
+  myPlacemark0 = new ymaps.Placemark([59.938945,30.323261], {
+    iconImageHref: 'img/icon-map-pin.svg',
+    iconImageSize: [70, 100],
+    iconImageOffset: [-35, -64]
+  });
+
+  myMap.geoObjects.add(myPlacemark0);
+}
